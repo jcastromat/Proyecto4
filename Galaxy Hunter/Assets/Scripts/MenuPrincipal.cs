1d66c1f8f29ad5c0a11874b05be79f542c4598f5
jcastromat <Javier@Javiers-MacBook-Air.local>
2016-06-03 03:33:58 -0600

    Menu
    Main and Pause Menu

---
+++ Galaxy Hunter/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPrincipal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame(){
+	
+		//Application.loadedLevel (0);
+	
+	}
+
+	public void NewGame(){
+		Application.LoadLevel("Scene1");
+	}
+
+	public void Quit(){
+	
+		Application.Quit ();
+		
+	}
+}
---
+++ Galaxy Hunter/Assets/Scripts/PauseMenu.js
@@ -0,0 +1,59 @@
+﻿var logoTexture : Texture2D;
+var newSkin : GUISkin;
+var PauseGame = false;
+
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Escape)){
+		PauseGame = !PauseGame;
+
+		if (PauseGame == true) {
+			Time.timeScale = 0;
+	        DisplayMenu();
+        }
+
+        if (PauseGame == false) {
+			Time.timeScale = 1;
+	        DisplayMenu();
+        }
+	}
+}
+
+
+function DisplayMenu()
+{
+    if(PauseGame == true){
+    GUI.BeginGroup(Rect(Screen.width / 2 - 150, 50, 300, 250));
+		
+		//the menu background box
+		GUI.Box(Rect(0, 0, 300, 250), "");
+		
+		//logo picture
+		GUI.Label(Rect(15, 10, 300, 68), logoTexture);
+
+		//resume game
+		if(GUI.Button(Rect(55, 100, 180, 40), "Continuar")) {
+		    PauseGame = false;
+		    Time.timeScale = 1;
+            DisplayMenu();
+		}
+
+		//exit to main menu
+        if(GUI.Button(Rect(55, 150, 180, 40), "Salir")) {
+        	Application.LoadLevel("MenuPrincipal");
+            }
+		
+		//layout end
+		GUI.EndGroup(); 
+    }
+}
+function OnGUI () {
+		//load GUI skin
+		GUI.skin = newSkin;
+		
+		//show the mouse cursor
+		Cursor.visible = true;
+		
+		// Menu Script
+		DisplayMenu();
+}
