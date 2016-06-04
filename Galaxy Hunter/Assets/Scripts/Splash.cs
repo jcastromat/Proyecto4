468e14b03ffe6f0993188408620a57df11c4fe67
jcastromat <Javier@Javiers-MacBook-Air.local>
2016-06-03 23:05:59 -0600

    Add Splash Screen

---
+++ Galaxy Hunter/Assets/Scripts/Splash.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Countdown");
+
+	}
+	
+
+	private IEnumerator Countdown(){
+		yield return new WaitForSeconds (5);
+		Application.LoadLevel ("MenuPrincipal");		
+	}
+}
