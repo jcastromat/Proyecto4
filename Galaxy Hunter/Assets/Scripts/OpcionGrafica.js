ac36ce7242111102af25f7b1cdcf6af2e68dcebe
kenneth8627 <kenneth8627@gmail.com>
2016-06-25 23:56:04 -0600

    Corrección Colores, Habilitación Opciones Gráficas

---
+++ Galaxy Hunter/Assets/Scripts/OpcionGrafica.js
@@ -0,0 +1,23 @@
+﻿public function GraficosBajos(){
+	QualitySettings.currentLevel = QualityLevel.Fastest;
+}
+
+public function GraficosSemiBajos(){
+	QualitySettings.currentLevel = QualityLevel.Fast;
+}
+
+public function GraficosMedioBajos(){
+	QualitySettings.currentLevel = QualityLevel.Simple;
+}
+
+public function GraficosNormales(){
+	QualitySettings.currentLevel = QualityLevel.Good;
+}
+
+public function GraficosAltos(){
+	QualitySettings.currentLevel = QualityLevel.Beautiful;
+}
+
+public function GraficosUltra(){
+	QualitySettings.currentLevel = QualityLevel.Fantastic;
+}
