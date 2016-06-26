6d6cc348ea9f0937de1b680a38988d0ea81b90e8
kenneth8627 <kenneth8627@gmail.com>
2016-06-25 20:51:02 -0600

    Examen4
    MultiJugador, UnJugador, Timer, EscogerTiempo de juego

--- Galaxy Hunter/Assets/Scripts/MenuPrincipal.cs
+++ Galaxy Hunter/Assets/Scripts/MenuPrincipal.cs
@@ -3,6 +3,21 @@ using System.Collections;
 
 public class MenuPrincipal : MonoBehaviour {
 
+	public float min = 0.0f;
+	public float seg = 0.0f;
+
+	public static MenuPrincipal menuP;
+
+	void Awake(){
+	
+		if (menuP == null) {
+			menuP = this;
+			DontDestroyOnLoad (gameObject);
+		} else
+			Destroy (gameObject);
+	
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -28,4 +43,23 @@ public class MenuPrincipal : MonoBehaviour {
 		Application.Quit ();
 		
 	}
+
+	public void cinco(){
+	
+		min = 5f;
+
+	}
+
+	public void dies(){
+	
+		min = 10f;
+
+	}
+
+	public void quince(){
+	
+		min = 15f;
+	
+	}
+
 }
---
+++ Galaxy Hunter/Assets/Scripts/TimerS.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var time :String;
+var maxTime :float;
+
+private var nextTime = 0.5;
+private var minutos = 0;
+private var segundos = 0;
+
+function Update () {
+
+	nextTime = Time.time;
+	minutos = nextTime/60;
+	segundos = nextTime%60;
+
+}
+
+function OnGUI(){
+	GUI.Box(new Rect(Screen.width/2 -100, 0, 200, 50), "Minutos: " +minutos +" Segundos " +segundos);
+
+}
