226668435ae7efa3c3aa3dabfbfd2c2beccf4be9
jcastromat <Javier@Javiers-MacBook-Air.local>
2016-08-01 13:37:12 -0600

    Locked and Unlocked Levels, Updated PowerUps Menu

--- Galaxy Hunter/Assets/Scripts/GUI Display.js
+++ Galaxy Hunter/Assets/Scripts/GUI Display.js
@@ -1,34 +1,34 @@
+#pragma strict
+
 var plane : Transform;
 
-public var item0 : GUIContent = GUIContent("");
-public var item1 : GUIContent = GUIContent("");
-public var item2 : GUIContent = GUIContent("Alt...");
 private var item7value = true;
 public var guiSkin : GUISkin; 
 var Alt:int;
-var Spd:int;
+var Destroyed:int;
 var Weapon:String;
 function OnGUI(){ 
 	GUI.skin = guiSkin;
 	GUI.Box(Rect(0, Screen.height-100, 220, 100),"Ship Stats");
-	GUI.Box(Rect(90, Screen.height-80, 120, 70), item0);
-	GUI.Box(Rect(10, Screen.height-80, 70, 70), item1);
+	GUI.Box(Rect(90, Screen.height-80, 120, 70), "");
+	GUI.Box(Rect(10, Screen.height-80, 70, 70), "");
 	GUI.Label(Rect(15, Screen.height-80, 100, 50), "Height");
-	GUI.Label(Rect(15, Screen.height-60, 100, 50), "Velicity");
+	GUI.Label(Rect(15, Screen.height-60, 100, 50), "Destroyed");
 	GUI.Label(Rect(15, Screen.height-40, 100, 50), "Weapon");
 	GUI.Label(Rect(100, Screen.height-80, 100, 50), Alt.ToString());
-	GUI.Label(Rect(100, Screen.height-60, 100, 50), Spd.ToString());
+	GUI.Label(Rect(100, Screen.height-60, 100, 50), Destroyed.ToString());
 	GUI.Label(Rect(100, Screen.height-40, 100, 50), Weapon);
 }
 
 
 function Update(){
 
-Alt=plane.transform.position.y;
-Spd=plane.GetComponent.<Rigidbody>().velocity.magnitude;
-Weapon=plane.GetComponent.<WeaponSwitching>().currentWeapon;
+	Alt=plane.transform.position.y;
+	Destroyed=GameObject.FindWithTag("PlayerManager").GetComponent.<ManageDestroyed>().destroyed;
+	Weapon=plane.GetComponent.<WeaponSwitching>().currentWeapon;
 
-var score = GameObject.FindWithTag("Score").GetComponent.<UnityEngine.UI.Text>();
-score.text= "SCORE:   " + GameObject.FindWithTag("PlayerManager").GetComponent.<ManageScore>().currentScore;
+	var score = GameObject.FindWithTag("Score").GetComponent.<UnityEngine.UI.Text>();
+	score.text= "SCORE:   " + GameObject.FindWithTag("PlayerManager").GetComponent.<ManageScore>().currentScore;
 
 }
+
---
+++ Galaxy Hunter/Assets/Standard Assets/ManageDestroyed.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var destroyed : int;
+
+function Start () {
+	GameObject.FindWithTag("Portal").GetComponent.<ParticleSystem>().Stop();
+	destroyed = 0; 
+}
+
+function Add() {
+	destroyed += 1;
+}
+
+function Update(){
+	if (destroyed >= 2){
+		GameObject.FindWithTag("Portal").GetComponent.<ParticleSystem>().Play();
+		GameObject.FindWithTag("Portal").GetComponent.<ManagePortal>().EnablePortal();
+	}
+
+}
