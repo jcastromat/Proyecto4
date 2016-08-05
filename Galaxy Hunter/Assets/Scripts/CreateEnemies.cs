bdd324a2831a6360c1964cb2654ac1961cd9aa39
jcastromat <Javier@Javiers-MacBook-Air.local>
2016-08-05 11:59:01 -0600

    Added Extra Enemies

--- Galaxy Hunter/Assets/Scripts/CreateEnemies.cs
+++ Galaxy Hunter/Assets/Scripts/CreateEnemies.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class CreateEnemies : MonoBehaviour {
 
 	public Transform spawn;
-	public GameObject enemyShip;
+	public GameObject enemyShip1;
+	public GameObject enemyShip2;
 	public int instanceCount = 5;
 
 	private void CreateInstances() {
@@ -13,12 +14,14 @@ public class CreateEnemies : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < instanceCount; ++i) {
+		for (int i = 0; i < instanceCount; i += 2) {
 			//Random.insideUnitCircle * 500
 
-			Vector3 position = new Vector3(Random.Range(spawn.position[0]-250, spawn.position[0]+250), Random.Range(spawn.position[1]-20, spawn.position[1]+20), Random.Range(spawn.position[2]-200, spawn.position[2]+200));
-			//Vector3 position = new Vector3(Random.Range(800f, 1100f), Random.Range(50f, 90f), Random.Range(1000f, 1200f));
-			GameObject instance = Instantiate (enemyShip, position , spawn.rotation) as GameObject;
+			Vector3 position1 = new Vector3(Random.Range(spawn.position[0]-250, spawn.position[0]+250), Random.Range(spawn.position[1]-20, spawn.position[1]+20), Random.Range(spawn.position[2]-200, spawn.position[2]+200));
+			GameObject instance1 = Instantiate (enemyShip1, position1 , spawn.rotation) as GameObject;
+
+			Vector3 position2 = new Vector3(Random.Range(spawn.position[0]-250, spawn.position[0]+250), Random.Range(spawn.position[1]-20, spawn.position[1]+20), Random.Range(spawn.position[2]-200, spawn.position[2]+200));
+			GameObject instance2 = Instantiate (enemyShip2, position2 , spawn.rotation) as GameObject;
 
 		}
 	}
